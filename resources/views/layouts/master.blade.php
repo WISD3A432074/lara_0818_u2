8d914212793b39b44ec85202da0d09e5aad37a2f
LeoHuang <leohuang1996@gmail.com>
2017-10-23 14:29:23 +0800

    [3A432074黃威銘]建立共用區塊

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,8 +3,9 @@
  <title>@yield('title') | My Blog</title>
  </head>
  <body>
+    @include('layouts.partials.navigation')
  <div class="container">
      @yield('content')
  </div>
  </body>
- </html> 
+ </html>
