7edbe28e0b81f6565af0eac3463165a5ca1d4e98
LeoHuang <leohuang1996@gmail.com>
2017-10-23 14:33:46 +0800

    [3A432074黃威銘]將參數從HelloController傳遞至view_hello.index

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,5 +6,9 @@
 
  class HelloController extends Controller
  {
-     //
+       public function index($name = 'Everybody')
+    {
+        $data = ['name' => $name];
+        return view('hello.index', $data);
+    }
  }
